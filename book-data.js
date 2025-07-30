a95f56c51ac643304054260f514a4e3ffa93120e
jaheen12 <110306558+jaheen12@users.noreply.github.com>
2025-07-30 23:31:42 +0600

    Add files via upload

---
+++ book-data.js
@@ -0,0 +1,112 @@
+// book-data.js
+
+const bookLibrary = [
+  {
+    category: "1. Calculus",
+    content: [
+      { name: "3000 Solved Problems in Calculus", link: '#' },
+      {
+        category: "Differential Calculus",
+        content: [
+          { name: "AOD-edited.pdf", link: '#' },
+          { name: "Differentiation-edited.pdf", link: '#' },
+          { name: "Limit-edited.pdf", link: '#' },
+        ]
+      },
+      { name: "Frank Ayres - Schaum's Outline of Calculus (2009)", link: '#' },
+      { name: "Frank Ayres - Schaum's Outline of Calculus (Media)", link: '#' },
+      {
+        category: "Integral Calculus",
+        content: [
+          { name: "Calculus of Variations with Applications", link: '#' },
+          { name: "Calculus of Variations (PDFDrive)", link: '#' },
+          { name: "Introduction to the Calculus of Variations", link: '#' },
+          { name: "Mathematics - Calculus of Variations with Applications", link: '#' },
+        ]
+      },
+      { name: "Schaum's - Understanding Calculus Concepts", link: '#' },
+      { name: "Schaum's - Advanced Calculus (1)", link: '#' },
+      { name: "Schaum's - Advanced Calculus", link: '#' },
+      { name: "Schaum's - Theory and Problems of Calculus", link: '#' },
+    ]
+  },
+  {
+    category: "2. Linear Algebra",
+    content: [
+      { name: "David Poole - Linear Algebra", link: '#' },
+      { name: "Determinant-edited.pdf", link: '#' },
+      { name: "Hadley - Linear Algebra", link: '#' },
+      { name: "Linda Gilbert - Linear Algebra", link: '#' },
+      { name: "Linear Algebra Done Right", link: '#' },
+      { name: "Schaum's - Linear Algebra 4th Ed", link: '#' },
+    ]
+  },
+  {
+    category: "3. Real Analysis",
+    content: [
+      { name: "Basic Real Analysis", link: '#' },
+      { name: "Chapman - Real Mathematical Analysis", link: '#' },
+      { name: "Elementary Real Analysis", link: '#' },
+      { name: "Bartle & Sherbert - Introduction to Real Analysis", link: '#' },
+      { name: "Rudin - Principles of Mathematical Analysis", link: '#' },
+      { name: "S.K. Mapa - Introduction to Real Analysis", link: '#' },
+    ]
+  },
+  {
+    category: "4. Abstract Algebra",
+    content: [
+        { name: "Abstract Algebra Schaums Outline 2nd Ed", link: '#' },
+        {
+            category: "Group Theory",
+            content: [
+                { name: "Group Theory Questions", link: '#' },
+                { name: "Schaums Outline - Group Theory", link: '#' },
+            ]
+        },
+        { name: "Schaum's - Abstract Algebra", link: '#' },
+    ]
+  },
+  {
+    category: "5. Complex Analysis",
+    content: [
+        { name: "Schaum's - Complex Variables 2nd Ed", link: '#' },
+        { name: "complex number-edited.pdf", link: '#' },
+    ]
+  },
+  {
+      category: "6. Differential Equations",
+      content: [
+          {
+              category: "Ordinary Differential Equations (ODE)",
+              content: [ { name: "Schaum's - Differential Equations", link: '#' } ]
+          },
+          {
+              category: "Partial Differential Equations (PDE)",
+              content: [ { name: "Schaum's - Partial Differential Equations", link: '#' } ]
+          }
+      ]
+  },
+  {
+    category: "9. Statistics and Probability",
+    content: [
+        { name: "Schaums - Probability, Random Variables, and Random Processes", link: '#' }
+    ]
+  },
+  {
+    category: "Other Resources",
+    content: [
+        {
+            category: "Question Banks & Exams",
+            content: [
+                { name: "5th Sem Question (Rajendra)", link: '#' },
+                { name: "DSE2 5th Sem", link: '#' },
+                { name: "JAM (2021-2005)", link: '#' },
+                { name: "TIFR Maths (2020-2010)", link: '#' },
+            ]
+        },
+        { name: "Schaum's - Mathematical Handbook of Formulas and Tables", link: '#' },
+        { name: "Schaum's - General Topology", link: '#' },
+        { name: "Schaum's - Vector Analysis", link: '#' },
+    ]
+  }
+];
